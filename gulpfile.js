dcae17f4437ce156460bce92a3fe5c3c08e0b949
Katya Savina <katerina.savina@yahoo.com>
2015-05-30 16:02:45 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp"),
-	browserSync = require('browser-sync');
+	wiredep = require("wiredep").stream,
+	browserSync = require('browser-sync'),
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css');
 
 // запуск локального сервера
 gulp.task('server', function () { 
@@ -21,3 +26,23 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['server', 'watch']);
+
+// прописывает bower зависимости
+gulp.task('wiredep', function () { 
+	gulp.src('app/*.html')
+		.pipe(wiredep())
+		.pipe(gulp.dest('app'));
+});
+
+// автоматизация сборки
+gulp.task('dist', function () {
+	var assets = useref.assets();
+
+	return gulp.src('app/*.html')
+	.pipe(assets)
+	.pipe(gulpif('*.js', uglify()))
+	.pipe(gulpif('*.css', minifyCss()))
+	.pipe(assets.restore())
+	.pipe(useref())
+	.pipe(gulp.dest('dist'));
+});
